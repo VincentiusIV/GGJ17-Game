3786cedc64bdc935dfd17abcd4a332e99e442628
dfrankes <davy@wdmsh.nl>
2017-01-21 00:19:04 +0100

    Added Basic GeneratorPulser

---
+++ Assets/Scripts/WaveController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour {
+
+    [SerializeField] private GeneratorController gc;
+    [SerializeField] private  CircleCollider2D collider;
+    [SerializeField] private float waveSpeed;
+    [SerializeField] private float currentRadius;
+    public bool isActive = false;
+
+
+    public void Setup()
+    {
+        waveSpeed = gc.GetDefaultWaveSpeed();
+        currentRadius = gc.GetDefaultWaveSize();
+        collider = GetComponent<CircleCollider2D>();
+    }
+
+    void Update()
+    {
+        if (isActive && collider.radius >= gc.GetMaxRadius())
+        {
+            collider.radius = gc.GetMinRadius();
+        }
+
+        if (isActive && collider.radius <= gc.GetMaxRadius())
+        {
+            collider.radius += waveSpeed;
+        }
+    }
+
+    public void SetRandomSpeed()
+    {
+        waveSpeed = Random.Range(0.01f, 0.25f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //
+    }
+}
