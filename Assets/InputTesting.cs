4ad7b1903b7c3bf69c368e68a63041f7916b6037
Vincent Versnel <vincent@versnelhifi.nl>
2017-01-20 20:51:45 +0100

    player controllers

--- Assets/InputTesting.cs
+++ Assets/InputTesting.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class InputTesting : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        if (Input.GetButtonDown("A_1"))
+            Debug.Log("Pressed A_1");
+        if (Input.GetButtonDown("A_2"))
+            Debug.Log("Pressed A_2");
+        if (Input.GetButtonDown("A_3"))
+            Debug.Log("Pressed A_3");
+        if (Input.GetButtonDown("A_4"))
+            Debug.Log("Pressed A_4");
+    }
 }
