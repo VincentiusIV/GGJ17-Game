9157a6e3c188fa58a4d9818c9fabc63321f245e4
dfrankes <davy@wdmsh.nl>
2017-01-22 00:42:06 +0100

    Remade...
    d

---
+++ Assets/NotificationManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationManager : MonoBehaviour {
+    [SerializeField] private Text NotificationText;
+    [SerializeField] private Image NotificationIcon;
+
+    [SerializeField] private GameObject panel;
+
+    [SerializeField] private string[] messageList;
+    [SerializeField] private Sprite[] iconList;
+
+    public void ShowNotificationBar(int _id)
+    {
+        GetComponent<Animation>().Play();
+        NotificationText.text = messageList[_id];
+        NotificationIcon.sprite = iconList[_id];
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShowNotificationBar(Random.Range(0, messageList.Length));
+        }
+    }
+}
