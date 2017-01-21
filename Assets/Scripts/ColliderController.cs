116f645fd0c00e2a1b586c1223d1d3ec3203db94
dfrankes <davy@wdmsh.nl>
2017-01-21 14:57:02 +0100

    New Wave Generator

---
+++ Assets/Scripts/ColliderController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class ColliderController : MonoBehaviour {
+
+    [SerializeField] private CircleCollider2D circleCollider;
+
+    public string targetTeamTag { get; set; }
+    public string colliderTag { get;set; }
+    public float baseSpeed { get; set; }
+    public float baseRadius { get; set; }
+    public float maxRadius { get; set; }
+    public Color color { get; set; }
+    public bool isActive { get; set; }
+    public bool repeat { get; set; }
+
+    private GeneratorController gc;
+    private DrawCircle drawCircle;
+
+
+    [SerializeField] private LineRenderer lr;
+
+
+    public void Setup()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+        lr = GetComponent<LineRenderer>();
+
+        circleCollider.radius = baseRadius;
+
+        //Give Random Color when spawned from custom event
+        if (!repeat)
+        {
+            lr.startColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+            lr.endColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+        }else
+        {
+            lr.startColor = color;
+            lr.endColor = color;
+        }
+
+
+        lr.startWidth = 0.1f;
+        lr.endWidth = 0.1f;
+
+        drawCircle = GetComponent<DrawCircle>();
+        drawCircle._horizRadius = 0.1f;
+        drawCircle._vertRadius = 0.1f;
+
+
+        isActive = true;
+    }
+
+    void Update()
+    {
+
+        if(isActive && circleCollider.radius >= maxRadius)
+        {
+            if (!repeat) {
+                isActive = false;
+                Destroy(this.gameObject, 0.5f);
+            }
+
+            drawCircle.random = Random.value;
+            drawCircle._horizRadius = baseRadius;
+            drawCircle._vertRadius = baseRadius;
+            circleCollider.radius = baseRadius;
+        }
+
+        if(isActive && circleCollider.radius <= maxRadius)
+        {
+            drawCircle._horizRadius += baseSpeed * 2;
+            drawCircle._vertRadius += baseSpeed * 2;
+            circleCollider.radius += baseSpeed;
+        }
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D _col)
+    {
+        if(_col.transform.tag == targetTeamTag)
+        {
+            //Do stuff when player gets hit by the "wave"
+
+        }
+    }
+
+
+    public void SetSpeed(float _speed)
+    {
+        baseSpeed = _speed;
+    }
+}
--- Assets/Scripts/GeneratorController.cs
+++ Assets/Scripts/GeneratorController.cs
@@ -4,52 +4,67 @@ using UnityEngine;
 
 public class GeneratorController : MonoBehaviour {
     [SerializeField] private string[] teamTags;
-    [SerializeField] private CircleCollider2D[] teamWaveColliders;
-    [SerializeField] private float defaultWaveSize;
-    [SerializeField] private float defaultWaveSpeed;
+    [SerializeField] private GameObject wavePrefab;
+
+    [SerializeField] private float baseSpeed;
+    [SerializeField] private float baseRadius;
     [SerializeField] private float maxRadius;
-    [SerializeField] private float minRadius;
-    [SerializeField] private GameObject particle;
+    [SerializeField] private string colliderTagPrefix;
 
     void Start()
     {
-        //Setup all wavColliders
-        for (int i = 0; i < teamWaveColliders.Length; i++)
+        foreach (var _team in teamTags)
         {
-            teamWaveColliders[i].GetComponent<WaveController>().Setup();
-            teamWaveColliders[i].GetComponent<WaveController>().isActive = true;
+            SpawnWave(_team, true);
         }
+
     }
+
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            teamWaveColliders[Random.Range(0, teamWaveColliders.Length)].GetComponent<WaveController>().SetRandomSpeed();
+            GameObject _comp = GameObject.Find("CC_TEAM_01");
+
+            _comp.GetComponent<ColliderController>().baseSpeed += 0.05f;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GameObject _comp = GameObject.Find("CC_TEAM_02");
+            _comp.GetComponent<ColliderController>().baseSpeed += 0.05f;
         }
 
-    }
-    public float GetDefaultWaveSize()
-    {
-        return defaultWaveSize;
-    }
-    public float GetDefaultWaveSpeed()
-    {
-        return defaultWaveSpeed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnWave("test", false);
+        }
     }
 
-    public float GetMaxRadius()
+    void SpawnWave(string _team, bool _repeat)
     {
-        return maxRadius;
-    }
+        GameObject _waveCollider = Instantiate(wavePrefab, transform.position, Quaternion.identity);
+        _waveCollider.transform.name = colliderTagPrefix + _team;
 
-    public float GetMinRadius()
-    {
-        return minRadius;
-    }
+        ColliderController _wcc = _waveCollider.GetComponent<ColliderController>();
 
-    public GameObject GetHitParticle()
-    {
-        return particle;
+        _wcc.targetTeamTag = _team;                  //Setup Collider Target Tag
+        _wcc.baseSpeed = baseSpeed;                  //Set Collider baseSpeed
+        _wcc.baseRadius = baseRadius;                //Set Collider base Radius
+        _wcc.maxRadius = maxRadius;                  //Set collider max Radius
+        _waveCollider.transform.parent = transform;  //Set parent to this object.
+        _wcc.repeat = _repeat;
+
+        switch (_team)
+        {
+            case "TEAM_01":
+                _wcc.color = Color.red;
+                break;
+            case "TEAM_02":
+                _wcc.color = Color.blue;
+                break;
+        }
+
+        _wcc.Setup();                               //Apply all settings
     }
 }
